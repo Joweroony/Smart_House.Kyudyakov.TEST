575c873602378582f03bb4d8d91e2f57041980ff
Кюдяков ИСИП20 <kudakovtimurisip20@iMac-Mac005-7.local>
2022-09-22 15:39:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/smart_housekyudyakov/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.smart_housekyudyakov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.enter_your_house);
+    }
+    public void start_new_resident(View v) {
+        Intent intent = new Intent(this, new_resident.class);
+        startActivity(intent);
+    }
+}
